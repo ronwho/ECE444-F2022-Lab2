17f3567f019c4d750950e229c9d830a85b586f41
ronwho <codingmonkron@gmail.com>
2022-09-25 04:19:36 -0400

    Finished up to example 4-7 part 1 of activity 4

--- hello.py
+++ hello.py
@@ -1,15 +1,34 @@
-from flask import Flask, render_template
+from tokenize import Name
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
-@app.route('/')
+# activity 4
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[InputRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html', curr_time = datetime.utcnow()) 
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form,name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
